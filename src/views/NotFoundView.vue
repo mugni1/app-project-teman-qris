2c3c72156a7f458205e6894e5af9733a8c30f82b
Mugni <asepabdulmugni22@gmail.com>
2026-01-23 10:00:08 +0700

    feat: add not-found-view

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Content from '@/components/content/Content.vue'
+</script>
+
+<template>
+  <Content>
+    <h1>Welcome Anjing</h1>
+    <p>Anjay mabar guys</p>
+  </Content>
+</template>
