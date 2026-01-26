a7d73fc24b2af99b13b7e87197e6b3b2347c3921
Mugni <asepabdulmugni22@gmail.com>
2026-01-26 11:07:28 +0700

    feat: implementation by id

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import TopUpView from '@/views/TopUpView.vue'
 import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import DetailOrderView from '@/views/DetailOrderView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,7 @@ const router = createRouter({
     { path: '/top-up/:slug', name: 'top_up', component: TopUpView },
     { path: '/login', name: 'auth_login', component: LoginView },
     { path: '/register', name: 'auth_register', component: RegisterView },
+    { path: '/detail/:id', name: 'detail_order', component: DetailOrderView },
     { path: '/:pathMatch(.*)*', name: 'not_found', component: NotFoundView },
   ],
 })
---
+++ src/views/DetailOrderView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Content from '@/components/content/Content.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+const id = route.params.id as string
+ 
+</script>
+
+<template>
+    <Content>
+        <h1>Anjay mabar bro {{ id }}</h1>
+    </Content>
+</template>
