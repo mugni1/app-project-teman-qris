ac3175f0b37fbc401b08049f9d4da272526482d0
Mugni <asepabdulmugni22@gmail.com>
2026-01-31 08:55:06 +0700

    refactor: improve color

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -5,7 +5,7 @@ import IconOmbak from '@/icons/IconOmbak.vue'
 <template>
   <IconOmbak class="text-base-300" />
   <footer class="bg-base-300 w-full">
-    <section class="container mx-auto p-4 space-y-4">
+    <section class="container mx-auto p-4 pb-8 space-y-8">
       <img src="https://topup.ebelanja.id/logo/logo-color.svg" alt="brand" class="h-12" />
       <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
         <div class="space-y-4 col-span-2 md:col-span-3 lg:col-span-2 w-full">
@@ -57,8 +57,8 @@ import IconOmbak from '@/icons/IconOmbak.vue'
           </div>
         </div>
       </div>
-      <div class="h-px w-full bg-base-300"></div>
-      <p class="text-center">
+      <div class="h-px w-full bg-base-content/70"></div>
+      <p class="text-center text-base-content/70">
         Copyright &copy; {{ new Date().getFullYear() }}. All Rights Reserved.
       </p>
     </section>
