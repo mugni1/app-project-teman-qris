122376c8e780a1738e0f7e01d80e9a76e6ebbaf8
Mugni <asepabdulmugni22@gmail.com>
2026-02-01 21:05:37 +0700

    refactor: improve not-found page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -12,7 +12,7 @@ const handleBack = () => {
 <template>
   <Content class="w-full min-h-screen flex items-center">
     <div class="flex flex-col items-center justify-center w-full h-full gap-5">
-      <img src="https://topup.ebelanja.id/404.svg" class="mx-auto mb-4 w-8/12" alt="" />
+      <img src="https://topup.ebelanja.id/404.svg" class="mx-auto mb-4 w-6/12" alt="" />
       <h2 class="text-center text-2xl font-semibold lg:text-3xl">Yah, Halaman ngga ada!</h2>
       <p class="text-center text-sm lg:text-base">
         Maaf ya! Halaman yang kamu cari ngga ada! Boleh dicoba lagi nanti.
