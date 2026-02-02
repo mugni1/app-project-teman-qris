5e241588f2505da2aca5dae4c79529561e4d8d22
Mugni <asepabdulmugni22@gmail.com>
2026-02-02 16:50:45 +0700

    feat: improve nav link

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -15,25 +15,30 @@ const token = Cookies.get('token')
         <img src="https://topup.ebelanja.id/logo/logo-color.svg" alt="brand" class="h-8" />
       </RouterLink>
       <div class="hidden md:flex text-sm gap-4 col-span-8 justify-center items-center">
-        <div v-for="item in data">
-          <RouterLink
-            v-if="!item.requireAuth && item.name != 'login'"
-            :to="item.link"
-            class="btn btn-ghost btn-primary btn-sm"
-          >
-            <component :is="item.icon" class="size-4" /> {{ item.title }}
-          </RouterLink>
-          <RouterLink v-if="item.requireAuth && token" :to="item.link" class="btn btn-ghost btn-primary btn-sm">
-            <component :is="item.icon" class="size-4" /> {{ item.title }}
-          </RouterLink>
-          <RouterLink
-            v-if="!item.requireAuth && item.name == 'login' && !token"
-            :to="item.link"
-            class="btn btn-ghost btn-primary btn-sm"
-          >
-            <component :is="item.icon" class="size-4" /> {{ item.title }}
-          </RouterLink>
-        </div>
+        <RouterLink
+          v-for="item in data"
+          v-show="!item.requireAuth && item.name != 'login'"
+          :to="item.link"
+          class="btn btn-ghost btn-primary btn-sm"
+        >
+          <component :is="item.icon" class="size-4" /> {{ item.title }}
+        </RouterLink>
+        <RouterLink
+          v-for="item in data"
+          v-show="item.requireAuth && token"
+          :to="item.link"
+          class="btn btn-ghost btn-primary btn-sm"
+        >
+          <component :is="item.icon" class="size-4" /> {{ item.title }}
+        </RouterLink>
+        <RouterLink
+          v-for="item in data"
+          v-show="!item.requireAuth && item.name == 'login' && !token"
+          :to="item.link"
+          class="btn btn-ghost btn-primary btn-sm"
+        >
+          <component :is="item.icon" class="size-4" /> {{ item.title }}
+        </RouterLink>
       </div>
       <div class="col-span-6 md:col-span-2 flex justify-end gap-2">
         <Thema />
