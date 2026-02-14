a8695d4b7853df91e30a5377f9e065dfe4c008a4
Mugni <asepabdulmugni22@gmail.com>
2026-02-14 21:02:50 +0700

    chore: improve header

--- src/components/games/Header.vue
+++ src/components/games/Header.vue
@@ -13,7 +13,7 @@ const props = defineProps<{
   <section class="relative card overflow-hidden bg-base-200 border border-base-300">
     <img :src="coverUrl" alt="banner" loading="lazy" class="h-45 lg:h-80 object-cover object-center" />
     <div class="gap-12 flex items-center p-4 lg:p-8">
-      <div class="ms-auto w-7/12 lg:w-10/12 lg:ps-10">
+      <div class="ms-auto w-7/12 md:w-8/12 lg:w-10/12 lg:ps-10">
         <h1 class="card-title text-base lg:text-xl line-clamp-1 truncate">{{ title }}</h1>
         <p class="stat-title lg:text-base">{{ studio }}</p>
         <div class="hidden lg:flex items-center gap-4 mt-4 card-title text-xs lg:text-sm">
@@ -28,7 +28,7 @@ const props = defineProps<{
       <span class="flex items-center gap-2"><ScanTextIcon class="size-4" /> Mudah</span>
       <span class="flex items-center gap-2"><Headphones class="size-4" /> CS 24 Jam</span>
     </div>
-    <div class="w-4/12 lg:w-2/12 absolute bottom-12 lg:bottom-8 left-4 lg:left-8 aspect-square">
+    <div class="w-4/12 md:w-3/12 lg:w-2/12 absolute bottom-12 md:bottom-13 lg:bottom-8 left-4 lg:left-8 aspect-square">
       <img
         :src="imageUrl"
         alt="image"
