bb1c2b7879240772d8e63e77cb330426b27037b9
Mugni <asepabdulmugni22@gmail.com>
2026-02-14 17:51:39 +0700

    chore: improve style

--- src/components/content/Content.vue
+++ src/components/content/Content.vue
@@ -1,7 +1,5 @@
 <template>
-    <main class="w-full py-4">
-        <section class="container mx-auto px-4">
-            <slot></slot>
-        </section>
-    </main>
+  <main class="w-full container mx-auto p-4">
+    <slot></slot>
+  </main>
 </template>
