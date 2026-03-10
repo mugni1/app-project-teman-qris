c7756637f59bbca01ab81ba7b53c292273422257
Mugni <asepabdulmugni22@gmail.com>
2026-03-10 21:36:33 +0700

    refactor: improve style

---
+++ src/components/news-detail/PendingNewsDetail.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import Content from '../content/Content.vue'
+</script>
+
+<template>
+  <Content class="space-y-4">
+    <h1 class="card-title text-lg lg:text-3xl skeleton text-transparent">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    </h1>
+    <div class="flex justify-between">
+      <span class="card-title text-xs md:text-sm text-transparent skeleton">Lorem, ipsum dolor.</span>
+      <span class="card-title text-xs md:text-sm text-transparent skeleton">Lorem, ipsum.</span>
+    </div>
+    <div class="w-full card aspect-video skeleton"></div>
+    <div class="space-y-4">
+      <p class="skeleton text-sm lg:text-base text-transparent">Lorem ipsum dolor, sit amet consectetur.</p>
+      <div class="space-y-1 text-sm lg:text-base">
+        <p class="skeleton w-full text-transparent">Lorem ipsum dolor</p>
+        <p class="skeleton w-full text-transparent">Lorem ipsum dolor</p>
+        <p class="skeleton w-full text-transparent">Lorem ipsum dolor</p>
+      </div>
+      <div class="space-y-1 text-sm lg:text-base">
+        <p class="skeleton w-full text-transparent">Lorem ipsum dolor</p>
+        <p class="skeleton w-full text-transparent">Lorem ipsum dolor</p>
+        <p class="skeleton w-full text-transparent">Lorem ipsum dolor</p>
+      </div>
+      <div class="space-y-1 text-sm lg:text-base">
+        <p class="skeleton w-full text-transparent">Lorem ipsum dolor</p>
+        <p class="skeleton w-full text-transparent">Lorem ipsum dolor</p>
+        <p class="skeleton w-full text-transparent">Lorem ipsum dolor</p>
+      </div>
+    </div>
+  </Content>
+</template>
--- src/views/NewsDetailView.vue
+++ src/views/NewsDetailView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import Content from '@/components/content/Content.vue'
+import PendingNewsDetail from '@/components/news-detail/PendingNewsDetail.vue'
+import { useRelativeTime } from '@/composables/useRelativeTime'
 import { useGetNewsDetail } from '@/hooks/useGetNewsDetail'
 import { useRoute } from 'vue-router'
 
@@ -9,7 +11,72 @@ const { data, isPending, isRefetching, refetch } = useGetNewsDetail(id)
 </script>
 
 <template>
-  <Content>
-    <h1>This Detail news</h1>
+  <PendingNewsDetail v-if="isPending" />
+  <Content v-if="!isPending && data && data.data && data.status == 200" :data="data.data" class="space-y-4">
+    <h1 class="card-title text-lg lg:text-3xl">{{ data?.data?.title }}</h1>
+    <div class="flex justify-between">
+      <span class="card-title text-xs md:text-sm">{{
+        new Date(data.data.created_at).toLocaleString('id-ID', {
+          day: '2-digit',
+          month: 'short',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+        })
+      }}</span>
+      <span class="card-title text-xs md:text-sm">{{ useRelativeTime(data.data.created_at).value }}</span>
+    </div>
+    <div>
+      <img
+        class="w-full bg-base-200 card aspect-video object-cover border border-base-300"
+        :src="data?.data?.image_url"
+        :alt="data?.data?.title"
+      />
+    </div>
+    <div class="ql-editor max-w-full text-sm lg:text-base prose" v-html="data?.data?.content"></div>
   </Content>
 </template>
+
+<style scoped>
+p {
+  margin: 14px 0;
+}
+
+p:empty {
+  height: 14px;
+}
+
+.ql-editor {
+  line-height: 1.7;
+  word-break: break-word;
+}
+
+/* ini penting */
+.ql-editor :deep(p) {
+  margin: 8px 0;
+}
+
+.ql-editor :deep(p:empty) {
+  height: 14px;
+}
+
+.ql-editor :deep(strong) {
+  font-weight: 600;
+}
+
+.ql-editor :deep(img) {
+  max-width: 100%;
+  height: auto;
+}
+
+.ql-editor :deep(table) {
+  display: block;
+  overflow-x: auto;
+}
+
+.ql-editor :deep(pre) {
+  overflow-x: auto;
+  padding: 12px;
+  border-radius: 6px;
+}
+</style>
