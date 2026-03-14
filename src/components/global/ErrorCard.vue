aa8822a52e97e8a9221f1ad11bd2c15bce94e539
Mugni <asepabdulmugni22@gmail.com>
2026-03-14 14:14:04 +0700

    refactor: improve style

--- src/components/global/ErrorCard.vue
+++ src/components/global/ErrorCard.vue
@@ -17,7 +17,7 @@ const refetch = () => {
 </script>
 
 <template>
-  <section class="flex justify-center items-center text-center py-8 bg-base-error/40 card border border-error">
+  <section class="flex justify-center items-center text-center py-8 bg-error/10 card border border-error">
     <h3 class="font-bold text-xl md:text-3xl text-error">{{ status }}</h3>
     <p v-if="props.status == 500" class="capitalize card-title text-sm text-error md:text-base">
       <TriangleAlertIcon class="size-4 md:size-5" /> Terjadi Kesalahan Pada Server.
